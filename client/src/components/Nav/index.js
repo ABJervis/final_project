2a82ce4e37f58a15078b019f19e56580f11d9c51
Blues <40874591+ABJervis@users.noreply.github.com>
2019-02-22 12:42:17 -0500

    navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import "./style.css";
 
 function Nav() {
   return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark border border-primary">
-        <a className="navbar-brand" href="./Home">Pediatric Appointment Tracker</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-item nav-link" href="./Login">Login</a>
-    </div>
-  </div>
-</nav>
+    <Navbar bg="dark" expand="lg">
+    <Navbar.Brand href="./Home">Pediatric Appointment Tracker</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Form inline>
+        <FormControl type="text" placeholder="Email Address" className="mr-sm-2" />
+        <FormControl type="text" placeholder="Password" className="mr-sm-2" />
+        <Button variant="outline-success">Login</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
   );
 }
+
+
 
 
 export default Nav;
